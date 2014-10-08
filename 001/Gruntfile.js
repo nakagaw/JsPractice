5ff6cd4c5db7438402b2cd7172dfea22e72a2af2
中川裕二 <yy.nw.yy@gmail.com>
2014-10-08 11:32:18 +0900

    やりはじめた

--- 001/Gruntfile.js
+++ 001/Gruntfile.js
@@ -1,9 +1,19 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     watch: {
-      files: ['stylesheets/*.css']
+      files: 'index.html', //監視したいファイルを指定
+      options: {
+        livereload: true //livereloadを有効
+      }
+    },
+    connect: {
+      uses_defaults: {} //空のサブタスクを入れます。名前は何でもいいです。ここではuses_defaultsにしました。
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.loadNpmTasks('grunt-contrib-watch');
+
+//これを指定しておくとgruntだけで実行できる
+grunt.registerTask('default', ['connect', 'watch']);
 };
